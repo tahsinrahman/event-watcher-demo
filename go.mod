b99f2dddfe62a1f5a2b59f42f5019dd7d4f8aba1
Md Tahsin Rahman <tahsynx@gmail.com>
2020-01-11 12:11:32 +0600

    Add event watcher

--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module github.com/tahsinrahman/event-watcher
 go 1.13
 
 require (
-	k8s.io/apimachinery v0.17.0 // indirect
-	k8s.io/client-go v11.0.0+incompatible
+	github.com/fatih/color v1.9.0 // indirect
+	github.com/hokaccha/go-prettyjson v0.0.0-20190818114111-108c894c2c0e // indirect
+	github.com/the-redback/go-oneliners v0.0.0-20190417084731-74f7694e6dae
+	k8s.io/api v0.0.0-20190918155943-95b840bb6a1f
+	k8s.io/apimachinery v0.17.0
+	k8s.io/client-go v0.0.0-20190918160344-1fbdaa4c8d90
 	sigs.k8s.io/controller-runtime v0.4.0
 )
--- main.go
+++ main.go
@@ -17,29 +17,56 @@ limitations under the License.
 package main
 
 import (
+	"context"
+	"fmt"
 	"os"
 
+	"github.com/the-redback/go-oneliners"
 	v1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func main() {
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{})
 	if err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	myReconciler := &eventReconciler{}
+	myReconciler := &eventReconciler{
+		client: mgr.GetClient(),
+	}
 
 	if err := ctrl.NewControllerManagedBy(mgr).For(&v1.Event{}).Complete(myReconciler); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
 
 type eventReconciler struct {
+	client client.Client
 }
 
 func (r *eventReconciler) Reconcile(request ctrl.Request) (ctrl.Result, error) {
+	fmt.Println(request.Name, request.Namespace)
+
+	event := &v1.Event{}
+	if err := r.client.Get(context.Background(), request.NamespacedName, event); err != nil {
+		if apierrors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	oneliners.PrettyJson(event)
+
 	return ctrl.Result{}, nil
 }
